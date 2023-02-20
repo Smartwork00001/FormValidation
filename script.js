7d3ce451435756a52ccf4c6dfc8297b90cbf9aaf
Teibok Chyne <smartwork00001@gmail.com>
2023-02-20 17:29:46 +0530

    form validate

--- script.js
+++ script.js
@@ -24,7 +24,19 @@ const submitButton = document.querySelector("#submit-button");
 const validateEmail = function () {
   if (emailInput.checkValidity()) {
     emailValidationMessageDiv.textContent = "";
+    if(emailInput.classList.contains("invalid")){
+        emailInput.classList.remove("invalid");
+    }
+    if(!emailInput.classList.contains("valid")){
+        emailInput.classList.add("valid");
+    }
   } else {
+    if(!emailInput.classList.contains("invalid")){
+        emailInput.classList.add("invalid");
+    }
+    if(emailInput.classList.contains("valid")){
+        emailInput.classList.remove("valid");
+    }
     emailValidationMessageDiv.textContent =
       "please enter valid email, e.g. smartwork@gmail.com";
   }
@@ -33,7 +45,19 @@ const validateEmail = function () {
 const validateCountry = function () {
   if (countryInput.checkValidity()) {
     countryValidationMessageDiv.textContent = "";
+    if(countryInput.classList.contains("invalid")){
+        countryInput.classList.remove("invalid");
+    }
+    if(!countryInput.classList.contains("valid")){
+        countryInput.classList.add("valid");
+    }
   } else {
+    if(!countryInput.classList.contains("invalid")){
+        countryInput.classList.add("invalid");
+    }
+    if(countryInput.classList.contains("valid")){
+        countryInput.classList.remove("valid");
+    }
     countryValidationMessageDiv.textContent =
       "please enter valid country name, must contain only letters";
   }
@@ -42,7 +66,19 @@ const validateCountry = function () {
 const validatePincode = function () {
   if (pincodeInput.checkValidity()) {
     pincodeValidationMessageDiv.textContent = "";
+    if(pincodeInput.classList.contains("invalid")){
+        pincodeInput.classList.remove("invalid");
+    }
+    if(!pincodeInput.classList.contains("valid")){
+        pincodeInput.classList.add("valid");
+    }
   } else {
+    if(!pincodeInput.classList.contains("invalid")){
+        pincodeInput.classList.add("invalid");
+    }
+    if(pincodeInput.classList.contains("valid")){
+        pincodeInput.classList.remove("valid");
+    }
     pincodeValidationMessageDiv.textContent =
       "please enter valid pincode, must be 6 digit number";
   }
@@ -56,17 +92,41 @@ const validatePassword = function () {
     )
   ) {
     passwordValidationMessageDiv.textContent = "";
+    if(passwordInput.classList.contains("invalid")){
+        passwordInput.classList.remove("invalid");
+    }
+    if(!passwordInput.classList.contains("valid")){
+        passwordInput.classList.add("valid");
+    }
   } else {
     passwordValidationMessageDiv.textContent = `please enter valid password
         Minimum eight characters, at least one uppercase letter, one lowercase letter, 
         one number and one special character:`;
+        if(!passwordInput.classList.contains("invalid")){
+            passwordInput.classList.add("invalid");
+        }
+        if(passwordInput.classList.contains("valid")){
+            passwordInput.classList.remove("valid");
+        }
   }
 };
 
 const validateConfirmation = function () {
   if (passwordInput.value === confirmationInput.value) {
     confirmationValidationMessageDiv.textContent = "";
+    if(confirmationInput.classList.contains("invalid")){
+        confirmationInput.classList.remove("invalid");
+    }
+    if(!confirmationInput.classList.contains("valid")){
+        confirmationInput.classList.add("valid");
+    }
   } else {
+    if(!confirmationInput.classList.contains("invalid")){
+        confirmationInput.classList.add("invalid");
+    }
+    if(confirmationInput.classList.contains("valid")){
+        confirmationInput.classList.remove("valid");
+    }
     confirmationValidationMessageDiv.textContent =
       "please enter the same password as above";
   }
@@ -74,6 +134,8 @@ const validateConfirmation = function () {
 
 const formSubmit = function(event){
     event.preventDefault();
+    const form = document.forms.validationForm;
+    form.reset();
 }
 
 emailInput.addEventListener("input", validateEmail);
